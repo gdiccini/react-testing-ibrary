c45c0ec42796582364d4968d252d066ae8d40666
giovani.diccini <giovani-diccini@pop-os.localdomain>
2021-03-18 18:11:29 -0300

    implements NotFound.js tests

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+// import React from 'react';
+// import About from '../components/About';
+// import renderWithRouter from '../renderWithRouter';
+
+// describe('Testa componente FavoritePokemons.js', () => {
+//   it('Testa que mensagem No favorite pokemon found', () => {
+
+//   });
+// });
