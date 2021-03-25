ed383f1568363993b3c04ecd8854eb02aa116e06
giovani.diccini <giovani-diccini@pop-os.localdomain>
2021-03-25 10:50:20 -0300

    fix isfavorit props on Pokemons.test.js tests

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,14 +1,14 @@
-import { objectOf } from 'prop-types';
 import React from 'react';
 import { Pokemon } from '../components';
 import pokemons from '../data';
 import renderWithRouter from '../renderWithRouter';
 
 describe('Testa componente Pokemons.js', () => {
+  const isTrue = true;
   it('Testa se é renderizado um card do pokemon', () => {
     const pikachu = pokemons[0];
     const { getByTestId, getByAltText } = renderWithRouter(
-      <Pokemon pokemon={ pikachu } isFavorite={ objectOf(true) } />,
+      <Pokemon pokemon={ pikachu } isFavorite={ isTrue } />,
     );
     const pokemonName = getByTestId('pokemon-name');
     expect(pokemonName.textContent).toBe('Pikachu');
@@ -23,7 +23,7 @@ describe('Testa componente Pokemons.js', () => {
   it('Testa se o link de navegação para os detalhes está correto', () => {
     const pikachu = pokemons[0];
     const { getByText } = renderWithRouter(
-      <Pokemon pokemon={ pikachu } isFavorite={ objectOf(true) } />,
+      <Pokemon pokemon={ pikachu } isFavorite={ isTrue } />,
     );
     const linkToDetails = getByText('More details');
     expect(linkToDetails).toBeInTheDocument();
@@ -32,7 +32,7 @@ describe('Testa componente Pokemons.js', () => {
   it('Testa se existe um ícone de estrela no Pokemon favoritado', () => {
     const pikachu = pokemons[0];
     const { getByAltText } = renderWithRouter(
-      <Pokemon pokemon={ pikachu } isFavorite={ objectOf(true) } />,
+      <Pokemon pokemon={ pikachu } isFavorite={ isTrue } />,
     );
     const favorited = getByAltText('Pikachu is marked as favorite');
     expect(favorited).toBeInTheDocument();
